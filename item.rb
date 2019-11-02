a6de9d56474c13f03847e0317897c58f19096633
Heidi <hengstrom25@gmail.com>
2019-11-02 12:31:02 -0400

    working POS

--- item.rb
+++ item.rb
@@ -1,4 +1,5 @@
 class Item
+attr_accessor :itemName, :unitPrice, :volumeAmount, :volumePrice
 
 	def initialize(itemName, unitPrice, volumeAmount, volumePrice)
 		@itemName = itemName
--- pointofsale.rb
+++ pointofsale.rb
@@ -1,13 +1,33 @@
+require './item'
 class PointOfSale
-require 'item'
 
-pricing = {}
-
-	def setPricing(itemName, unitPrice, volumeAmount=0, volumePrice=0)
-		pricing[itemName] = Item.new(itemName, unitPrice, volumeAmount, volumePrice)
+	def initialize
+		@pricing = {}
 	end
 
+	def setPricing(itemName, unitPrice, volumeAmount=0, volumePrice=0)
+		@pricing[itemName] = Item.new(itemName, unitPrice, volumeAmount, volumePrice)
+	end
 
-
-
+	def scan(items)
+		count = {}
+		total = 0 
+		items.chars.each{|i| 
+			if count.include?(i)
+				count[i] = count[i]+1
+			else
+				count[i] = 1
+			end}
+		@pricing.each{|itemName, item| 
+			if count.include?(itemName)
+				if item.volumeAmount > 0 && count[itemName] >= item.volumeAmount
+					volumeCount = count[itemName]/item.volumeAmount
+					leftoverCount = count[itemName] - volumeCount*item.volumeAmount
+					total += (volumeCount * item.volumePrice) + (leftoverCount * item.unitPrice)
+				else
+					total += count[itemName] * item.unitPrice
+				end
+			end}
+		total
+	end
 end
